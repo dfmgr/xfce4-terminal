f407273ab527dd44f54f6f9a501e2dd71ad54926
casjay <git-admin@casjaysdev.com>
2023-04-03 12:34:13 -0400

    🗃️  Committing everything that changed 🗃️

--- install.sh
+++ install.sh
@@ -148,6 +148,7 @@ run_postinst() {
   dfmgr_run_post
   [ -d "$HOME/.config/xfce4/terminal" ] || mkdir -p "$HOME/.config/xfce4/terminal"
   [ -d "$HOME/.local/share/xfce4/terminal/colorschemes" ] || mkdir -p "$HOME/.local/share/xfce4/terminal/colorschemes"
+  cp -Rf "$APPDIR/colorschemes/." "$HOME/.local/share/xfce4/terminal/colorschemes/"
   cp -Rf "$APPDIR/." "$HOME/.config/xfce4/terminal/"
 }
 #
